794ec1b7e4a07f0a0cb1a1a92c56f4020ad842a1
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2024-12-13 21:42:43 +0100

    Refactor App component to implement theme toggling and queries.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { MdOutlineLightMode } from "react-icons/md";
+import styled from "styled-components";
 
+const ThemeButton = styled.button<{ isLightTheme: boolean }>`
+  background-color: ${(props) => (props.isLightTheme ? "blue" : "black")};
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 5px;
+`;
+
+const queryClient = new QueryClient();
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [isLightTheme, setIsLightTheme] = useState(true);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <QueryClientProvider client={queryClient}>
+        <div className={`App ${isLightTheme ? "light" : "dark"}`}>
+          <ThemeButton
+            isLightTheme={isLightTheme}
+            onClick={() => setIsLightTheme(!isLightTheme)}
+          >
+            <MdOutlineLightMode />
+          </ThemeButton>
+        </div>
+      </QueryClientProvider>
     </>
   );
 }

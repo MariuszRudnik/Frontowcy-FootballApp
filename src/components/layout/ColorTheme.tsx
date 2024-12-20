064a703c54b5c5c9c0906ec5881e5e40be67b0bc
mrjson <mariuszjson@gmail.com>
2024-12-20 21:59:09 +0100

    Add .env to .gitignore
    Exclude .env files from version control to prevent sensitive information from being committed. This ensures better security and adherence to best practices.

--- src/components/layout/ColorTheme.tsx
+++ src/components/layout/ColorTheme.tsx
@@ -5,7 +5,6 @@ const FullScreenDiv = styled.div<{ isLightTheme: boolean }>`
   min-height: 100vh;
   padding-bottom: 10px;
   margin: 0;
-
   color: #222222;
 
   background-color: ${(props) => (props.isLightTheme ? "#3c8546" : "gray")};
